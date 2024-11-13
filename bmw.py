2054e13976a82f3d277e7d0ec3f2c92d2888f115
Brian <chsieh06@usc.edu>
2024-11-13 01:46:02 -0800

    bmw_minAreaRect

--- bmw.py
+++ bmw.py
@@ -23,27 +23,6 @@ time.sleep(2.0)
 # Define color limits and threshold for ball height
 height_threshold = 350
 
-class KalmanFilter:
-    def __init__(self):
-        self.Q = 1e-5  # 過程雜訊協方差
-        self.R = 0.1   # 測量雜訊協方差
-        self.P = 1.0   # 估計誤差協方差
-        self.x = 0.0   # 狀態變數 (角度)
-        self.K = 0.0   # 卡爾曼增益
-
-    def update(self, measurement):
-        # 預測步驟
-        self.P = self.P + self.Q
-        
-        # 更新步驟
-        self.K = self.P / (self.P + self.R)
-        self.x = self.x + self.K * (measurement - self.x)
-        self.P = (1 - self.K) * self.P
-        return self.x
-
-# 初始化卡爾曼濾波器
-kf = KalmanFilter()
-
 def get_red_mask(hsv_frame):
     lower_red1 = np.array([0, 50, 50])
     upper_red1 = np.array([15, 255, 255])
@@ -100,8 +79,31 @@ def get_angular_velocity(degrees):
 def miniDistance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
+window_size = 10
+velocity_buffer = []
+def moving_average(velocity, buffer, window_size):
+    if len(buffer) == window_size:
+        mean = np.mean(buffer)
+        std_dev = np.std(buffer)
+
+        if abs(velocity - mean) > 5 * std_dev:
+            buffer.append(buffer[-1])
+        else:
+            buffer.append(velocity)
+    else:
+        buffer.append(velocity)
+
+    # 如果緩衝區超過窗口大小，則移除最舊的元素
+    if len(buffer) > window_size:
+        buffer.pop(0)
+    
+    # 計算移動平均
+    avg_velocity = sum(buffer) / len(buffer)
+    return avg_velocity
+
 angles = []  # List to store angles for plotting
 velocities = []
+mv_velocities = []
 angle = None
 prev_angle = None
 
@@ -157,62 +159,89 @@ while True:
             largest_center = None
             largest_area = 0
             largest_radius = 0
+            largest_rect = None
             for mc in mark_cnts:
                 area = cv2.contourArea(mc)
                 if area < 300:  # 忽略面積過小的 contours
                     continue
                 # Calculate the white sticker's center and radius
-                ((x_w, y_w), radius_w) = cv2.minEnclosingCircle(mc)
-                M_w = cv2.moments(mc)
-                mark_center = (int(M_w["m10"] / M_w["m00"]), int(M_w["m01"] / M_w["m00"]))  # Calculate the center
-                mark_center_distance = miniDistance(mark_center, center)
+                # ((x_w, y_w), radius_w) = cv2.minEnclosingCircle(mc)
+                # M_w = cv2.moments(mc)
+                # mark_center = (int(M_w["m10"] / M_w["m00"]), int(M_w["m01"] / M_w["m00"]))  # Calculate the center
+                # mark_center_distance = miniDistance(mark_center, center)
 
-                # Record mark center if within distance and radius threshold
-                if mark_center_distance < radius and radius_w > 10:
-                    area = cv2.contourArea(mc)
-                    if area > largest_area:
-                        # Update largest contour and area if this one is bigger
-                        largest_contour = mc
-                        largest_area = area
-                        largest_radius = radius_w
-                        largest_center = mark_center
+                # # Record mark center if within distance and radius threshold
+                # if mark_center_distance < radius and radius_w > 10:
+                #     area = cv2.contourArea(mc)
+                #     if area > largest_area:
+                #         # Update largest contour and area if this one is bigger
+                #         largest_contour = mc
+                #         largest_area = area
+                #         largest_radius = radius_w
+                #         largest_center = mark_center
 
-            if largest_contour is not None and len(c) >= 5:
-                num_sections = 4
-                ellipse = cv2.fitEllipse(largest_contour)
+                rect = cv2.minAreaRect(mc)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                # 畫出旋轉矩形
 
-                if counter % 10 == 0:
-                    print(ellipse)
-                angle = ellipse[2]
-                # angle = kf.update(angle) # use Kalman Filter
-                angles.append(angle)  # Store the angle for plotting
+                # 計算矩形面積
+                rect_area = cv2.contourArea(box)  # 這是矩形的面積
+
+                # 比例檢查：如果標記輪廓的面積與矩形的面積比率小於 min_area_ratio，則過濾掉
+                area_ratio = area / rect_area
+
+                if area_ratio < 0.7:
+                    continue  # 面積比例小於閾值，跳過此輪廓
+
+                cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
+
+                if rect_area > largest_area:
+                    largest_contour = mc
+                    largest_rect = rect
+
+            if largest_contour is not None and len(c) >= 5 and counter % 5 == 0:
+                # num_sections = 4
+                # ellipse = cv2.fitEllipse(largest_contour)
+                # ellipse_center, ellipse_axes, ellipse_angle = ellipse
+
+                # if counter % 10 == 0:
+                #     print(ellipse)
+                angles.append(largest_rect[2])  # Store the angle for plotting
+                print(largest_rect[2])
 
                 if prev_angle is not None:
-                    velocity = get_angular_velocity(angle - prev_angle)
-                    # if abs(velocity) > 10: 
-                    #     if velocities:
-                    #         velocity = velocities[-1]
-                    #     else:
-                    #         velocity = 0
+                    velocity = get_angular_velocity(largest_rect[2] - prev_angle)
+                    if abs(velocity) > 10: 
+                        if velocities:
+                            velocity = velocities[-1]
+                        else:
+                            velocity = 0
                     velocities.append(velocity)
-                prev_angle = angle
 
-                section_angle = 360 / num_sections
-                for i in range(num_sections):
-                    start_angle = int(angle + i * section_angle)
-                    end_angle = int(angle + (i + 1) * section_angle)
-                    section_color = (0, 0, 0) if i % 2 == 0 else (0, 255, 255)
-                    cv2.ellipse(frame, mark_center, (int(largest_radius), int(largest_radius)), 0, start_angle, end_angle, section_color, -1)
+                    mv_velocity = moving_average(velocity, velocity_buffer, window_size)
+                    mv_velocities.append(mv_velocity)
+                prev_angle = largest_rect[2]
+
+            #     ellipse_center = (int(ellipse_center[0]), int(ellipse_center[1]))
+            #     major_axis, minor_axis = int(ellipse_axes[0] // 2), int(ellipse_axes[1] // 2)
+
+            #     section_angle = 360 / num_sections
+            #     for i in range(num_sections):
+            #         start_angle = int(ellipse_angle + i * section_angle)
+            #         end_angle = int(ellipse_angle + (i + 1) * section_angle)
+            #         section_color = (0, 0, 0) if i % 2 == 0 else (0, 255, 255)
+            #         cv2.ellipse(frame, ellipse_center, (major_axis, minor_axis), 0, start_angle, end_angle, section_color, -1)
 
     counter += 1
     pts.appendleft(center)
     cv2.imshow("Ball Tracking", frame)
-    cv2.imshow('None Red mask', non_red_mask)
-    cv2.imshow('Red mask', mask)
+    # cv2.imshow('None Red mask', non_red_mask)
+    # cv2.imshow('Red mask', mask)
 
-    mean_velocity = np.mean(velocities)
-    measurement_noise_covariance = np.var(velocities, ddof=1)
-    print("variance (R):", measurement_noise_covariance)
+    # mean_velocity = np.mean(velocities)
+    # measurement_noise_covariance = np.var(velocities, ddof=1)
+    # print("variance (R):", measurement_noise_covariance)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
@@ -230,6 +259,13 @@ ax1.legend()
 
 ax2.plot(range(len(velocities)), velocities, label="Velocity")
 ax2.set_title("Velocity of Ball Over Time")
+ax2.set_xlabel("Frame")
+ax2.set_ylabel("Velocity (radians/sec)")
+ax2.grid(True)
+ax2.legend()
+
+ax2.plot(range(len(mv_velocities)), mv_velocities, label="Moving Average Velocity")
+ax2.set_title("Moving Average Velocity of Ball Over Time")
 ax2.set_xlabel("Frame")
 ax2.set_ylabel("Velocity (radians/sec)")
 ax2.grid(True)
